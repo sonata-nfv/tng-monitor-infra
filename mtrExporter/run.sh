667c1d47c6c79bfe161e63e33a209b41ad02d9df
Panagiotis Karkazis <pkarkazis@synelixis.com>
2018-08-23 12:47:20 +0300

    add service supervisor

--- mtrExporter/run.sh
+++ mtrExporter/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cat <(crontab -e) <(echo "*/1 * * * * /opt/Monitoring/checkservice.sh >> /var/log/task.log 2>&1") | crontab - & \
+cron & \
 python3 /opt/Monitoring/ceilExporter.py & \
 tail -f /dev/null
